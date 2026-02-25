d2e6c1951ea938f2649c2f950330f43ac1930622
Floww <dyon.schouten@icloud.com>
2026-02-25 23:19:10 +0100

    final fix

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  experimental: {
-    serverComponentsExternalPackages: ['@prisma/client', 'prisma'],
-  },
   images: {
     domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com'],
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client'],
   },
 }
 
